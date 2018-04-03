99799e9ea47ab20656ce328a732c644e84fea6f7
gustavoktausend <gustavoktausend@gmail.com>
2018-04-03 13:35:18 -0300

    first commit

---
+++ src/main/java/com/rest/springbootangular/SpringBootAngularRestApplication.java
@@ -0,0 +1,13 @@
+package com.rest.springbootangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularRestApplication.class, args);
+		
+	}
+}
