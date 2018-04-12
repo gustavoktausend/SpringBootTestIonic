b439c7e42f441ba8025dc41fb5b209bc06a573eb
gustavoktausend <gustavoktausend@gmail.com>
2018-04-12 14:22:44 -0300

    adicionando CategoriaDTO e Resource para busca de todas categorias

--- src/main/java/com/rest/springbootangular/resources/CategoriaResource.java
+++ src/main/java/com/rest/springbootangular/resources/CategoriaResource.java
@@ -1,7 +1,8 @@
 package com.rest.springbootangular.resources;
 
 import java.net.URI;
-import java.util.Optional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rest.springbootangular.domain.Categoria;
+import com.rest.springbootangular.dto.CategoriaDTO;
 import com.rest.springbootangular.services.CategoriaService;
 
 @RestController
@@ -49,6 +51,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List <Categoria> list = service.findAll();
+		List <CategoriaDTO> listDto =  list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 	
--- src/main/java/com/rest/springbootangular/services/CategoriaService.java
+++ src/main/java/com/rest/springbootangular/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.rest.springbootangular.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,6 +44,10 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria com produtos vinculados");
 			
 		}
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 
 }
